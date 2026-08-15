ca83974a9c4b50d3425cf138c45cc372eacbad09
TheLittleNestPlayschool <admin@thelittle-nest.com>
2026-08-16 00:24:44 +0800

    Create inquiry_elements.js

---
+++ inquiry_elements.js
@@ -0,0 +1,53 @@
+/* ==========================================
+   ELEMENTS
+========================================== */
+
+export const newInquiryButton =
+    document.getElementById(
+        'newInquiryButton'
+    );
+
+export const inquiryModal =
+    document.getElementById(
+        'inquiryModal'
+    );
+
+export const closeInquiryModal =
+    document.getElementById(
+        'closeInquiryModal'
+    );
+
+export const cancelInquiryButton =
+    document.getElementById(
+        'cancelInquiryButton'
+    );
+
+export const inquiryForm =
+    document.getElementById(
+        'inquiryForm'
+    );
+
+export const createInquiryButton =
+    document.getElementById(
+        'createInquiryButton'
+    );
+
+export const parentNameInput =
+    document.getElementById(
+        'parentName'
+    );
+
+export const franchiseSelect =
+    document.getElementById(
+        'franchiseSelect'
+    );
+
+export const inquiryPanel =
+    document.getElementById(
+        'inquiryPanel'
+    );
+
+export const emptyState =
+    document.getElementById(
+        'emptyState'
+    );
